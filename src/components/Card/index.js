a08e53d99d28060a72de87967e145e1660b109cb
ice-rahul <meet4g@gmail.com>
2021-06-11 13:22:32 +0530

    added enlarged graph

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,25 @@
 import React, { useMemo, useRef } from 'react';
 import PropTypes from 'prop-types';
 import getAQILevel from 'utils/getAQILevel';
-import { Sparklines, SparklinesLine, SparklinesSpots } from 'react-sparklines';
+import {
+  Sparklines, SparklinesCurve, SparklinesSpots, SparklinesReferenceLine,
+} from 'react-sparklines';
 
 const roundAQI = (aqi) => (Math.round(aqi * 100) / 100);
 
-function Card({ city, aqi }) {
+function Card({ city, aqi, onClick }) {
   const status = getAQILevel(roundAQI(aqi));
   const sampleData = useRef([]);
   sampleData.current = useMemo(() => [...sampleData.current, aqi], [aqi]);
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <div className="d-flex flex-column min-height-100px border-light border-radius-4px box-shadow-blue p-10px min-width-300px">
+    <div className="d-flex flex-column min-height-100px border-light border-radius-4px box-shadow-blue p-10px min-width-300px w-90-small" onClick={handleClick} aria-hidden>
       <span className="text-bold text-1-2-5em">{city}</span>
       <div className="mt-5px d-flex justify-space-between">
         <span>
@@ -20,9 +29,15 @@ function Card({ city, aqi }) {
         <span className="text-bold text-blue">{status && status.name}</span>
         <span className={`badge ${(status && status.className) || ''} box-shadow-blue mr-5px`}>&emsp;</span>
       </div>
-      <Sparklines data={sampleData.current}>
-        <SparklinesLine style={{ fill: 'none' }} />
+      <Sparklines limit={50} data={sampleData.current}>
+        <SparklinesCurve style={{ fill: 'none' }} />
         <SparklinesSpots />
+        <SparklinesReferenceLine
+          value={150}
+          style={{
+            stroke: 'yellow', strokeLinecap: 'round', strokeOpacity: 0.75, strokeDasharray: '2, 2',
+          }}
+        />
       </Sparklines>
     </div>
   );
@@ -31,11 +46,13 @@ function Card({ city, aqi }) {
 Card.propTypes = {
   city: PropTypes.string,
   aqi: PropTypes.number,
+  onClick: PropTypes.func,
 };
 
 Card.defaultProps = {
   city: '',
   aqi: 0,
+  onClick: null,
 };
 
 export default Card;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,37 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
 import React, { useState, useRef, useMemo } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import { Card } from 'components';
 import mergeMaps from 'utils/mergeMaps';
 import mapData from 'utils/mapData';
 
-const showList = (data) => {
-  const sortedArray = (Array.from(data)).sort((a, b) => {
-    if (a[0] < b[0]) {
-      return -1;
-    }
-    if (a[0] > b[0]) {
-      return 1;
-    }
-    return 0;
+const sortList = (data) => (Array.from(data)).sort((a, b) => {
+  if (a[0] < b[0]) {
+    return -1;
+  }
+  if (a[0] > b[0]) {
+    return 1;
+  }
+  return 0;
+});
+
+const handleSize = (containerId) => {
+  const allContainer = document.querySelectorAll("[id^='container']");
+  allContainer.forEach((val) => {
+    document.getElementById(val.id).style.order = '0';
+    document.getElementById(val.id).classList.remove('w-90');
   });
+  document.getElementById(containerId).style.order = '-1';
+  document.getElementById(containerId).classList.add('w-90');
+  window.scrollTo({ top: 0 });
+};
+
+const showList = (data) => {
+  const sortedArray = sortList(data);
   return sortedArray.map((val) => (
-    <div key={val[0]}>
-      <Card city={val[0]} aqi={val[1]} />
+    <div key={val[0]} id={`container-${val[0]}`} className="cursor-pointer transition-all w-100-small" aria-hidden>
+      <Card city={val[0]} aqi={val[1]} onClick={() => handleSize(`container-${val[0]}`)} />
     </div>
   ));
 };
@@ -27,7 +42,12 @@ function Home() {
   const {
     lastMessage,
     readyState,
-  } = useWebSocket(socketUrl);
+  } = useWebSocket(socketUrl, {
+    shouldReconnect: () => true,
+    reconnectAttempts: 10,
+    reconnectInterval: 1000,
+  });
+
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
     [ReadyState.OPEN]: 'Open',
@@ -35,11 +55,15 @@ function Home() {
     [ReadyState.CLOSED]: 'Closed',
     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
   }[readyState];
+
   cityList.current = useMemo(() => {
     const { data } = lastMessage || { data: '' };
     const JSONData = (data && JSON.parse(data));
+
     if (!data) return '';
+
     if (cityList.current.length === 0) return mapData(JSONData);
+
     const mergedMap = mergeMaps(cityList.current, mapData(JSONData));
     return mergedMap;
   }, [lastMessage]);
